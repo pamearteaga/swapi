e03cf7b912a65d91c361afd22a363407985204af
pamela <pamela@MacBook-Pro-de-pamela.local>
2018-02-15 10:41:35 -0300

    función llamada y modal

--- assest/js/app.js
+++ assest/js/app.js
@@ -2,12 +2,15 @@
 $('.personaje').click(function(){
 
   const img = $(this).val();
+
 $.ajax({
   url: `https://swapi.co/api/people/${img}`
 }).done(handleResponse).fail(errorResponse);
 
   function handleResponse(data) {
-    $('main').append('<div><h2>' + data.name + '</h2><h3>Born in ' + data.birth_year + '</h3><p>Gender: ' + data.gender + '</p><p>Height: ' + data.height + '</p><p>Eyes color: ' + data.eye_color + '</p><p>Hair color : ' + data.hair_color + '</p><p>Skin color: ' + data.skin_color + '</p>');
+    $('.modal-title, .modal-body').empty();
+    $('.modal-title').append('<h2>' + data.name + '</h2>');
+    $('.modal-body').append('<div><h4>Born in ' + data.birth_year + '</h4><p>Gender: ' + data.gender + '</p><p>Height: ' + data.height + '</p><p>Eyes color: ' + data.eye_color + '</p><p>Hair color : ' + data.hair_color + '</p><p>Skin color: ' + data.skin_color + '</p>');
   };
 
   function errorResponse() {
@@ -15,21 +18,3 @@ $.ajax({
   }
 
 });
-
-
-
-
-/*
-name
-birth_year
-eye_color
-gender
-hair_color
-height
-skin_color
-//llamadas
-homeworld/planets/name
-species/name
-starships/name
-vehicles/name
-*/
